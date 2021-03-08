4a1b6366f0da47bab3fced720fd1893cd698aed9
Daysha Jones <daysha.vu@icloud.com>
2021-03-08 21:40:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,5 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'ingido', 'violet']
 
 def display_rainbow(colors)
-  puts "#{colors[0]}"
-  puts "#{colors[1]}"
-  puts "#{colors[2]}"
-  puts "#{colors[3]}"
-  puts "#{colors[4]}"
-  puts "#{colors[5]}"
-  puts "#{colors[6]}"
+  puts colors
 end
